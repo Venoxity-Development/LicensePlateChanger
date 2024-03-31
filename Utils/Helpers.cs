f33152408d7afd961f99705e1ea182551c621210
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-03-30 20:03:12 -0700

    dont know

--- Utils/Helpers.cs
+++ Utils/Helpers.cs
@@ -5,24 +5,25 @@ namespace LicensePlateChanger.Utils
 {
     internal static class Helpers
     {
+        private static readonly Random Random = new Random(Guid.NewGuid().GetHashCode());
+
         /// <summary>
         /// Transforms a string by replacing '#' with a random digit and '?' with a random uppercase letter.
         /// </summary>
         public static string TransformString(string input)
         {
-            Random random = new Random();
             StringBuilder result = new StringBuilder();
 
             foreach (char c in input)
             {
                 if (c == '#')
                 {
-                    int randomNumber = random.Next(10);
+                    int randomNumber = Random.Next(10); 
                     result.Append(randomNumber);
                 }
                 else if (c == '?')
                 {
-                    char randomChar = (char)random.Next('A', 'Z' + 1);
+                    char randomChar = GetUniqueRandomChar();
                     result.Append(randomChar);
                 }
                 else
@@ -32,6 +33,16 @@ namespace LicensePlateChanger.Utils
             }
 
             return result.ToString();
+        }
+
+        private static char GetUniqueRandomChar()
+        {
+            char randomChar;
+            do
+            {
+                randomChar = (char)Random.Next('A', 'Z' + 1);
+            } while (randomChar == 'I' || randomChar == 'O'); // Exclude 'I' and 'O' to avoid confusion with '1' and '0'
+            return randomChar;
         }
     }
 }

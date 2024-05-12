9e2f630254b5a0fa7198960d55023625cd1af885
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-11 22:52:19 -0700

    New Instance System for Core

--- Main.cs
+++ Main.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using GTA;
-using GTA.Native;
+using LicensePlateChanger.Threads;
 using LicensePlateChanger.Utils;
 
 namespace LicensePlateChanger
@@ -10,6 +10,7 @@ namespace LicensePlateChanger
     public class Main : Script
     {
         #region Fields
+        private static VehicleManager VehicleManager = null;
         private static readonly Dictionary<string, DecoratorType> decorators = new Dictionary<string, DecoratorType>()
         {
             { "excludeVehicle", DecoratorType.Bool },
@@ -21,8 +22,21 @@ namespace LicensePlateChanger
         {
             Decorators.Initialize();
 
+            if (VehicleManager == null)
+            {
+                VehicleManager = InstantiateScript<VehicleManager>();
+            }
+            else
+            {
+                VehicleManager.Abort();
+
+                VehicleManager = null;
+            }
+
             Tick += OnInit;
             Aborted += OnAborted;
+
+            Interval = 1000;
         }
         #endregion
 
@@ -43,48 +57,6 @@ namespace LicensePlateChanger
             Configuration.LoadConfiguration();
 
             Tick -= OnInit;
-            Tick += OnTick;
-        }
-
-        private void OnTick(object sender, EventArgs e)
-        {
-            Wait(1000);
-            Vehicle[] nearbyVehicles = World.GetAllVehicles();
-            foreach (Vehicle val in nearbyVehicles)
-            {
-                if (val.Exists())
-                {
-                    int vehicleID = val.Handle;
-                    if (!Globals.vehicleLicensePlates.ContainsKey(vehicleID))
-                    {
-                        if (!Function.Call<bool>(Hash.DECOR_EXIST_ON, val, "excludeVehicle"))
-                        {
-                            if (Configuration.VehicleClassMapping.ContainsValue((VehicleClass)val.ClassType))
-                            {
-                                //if (!VehicleExtensions.IsVehicleExcluded(val) && !Function.Call<bool>(Hash.DECOR_GET_BOOL, val, "excludeVehicle"))
-                                //{
-                                //    string currentPlate = val.Mods.LicensePlate;
-                                //    string newPlate = VehicleExtensions.GetPlateFormatForVehicleClass(val);
-
-                                //    if (!string.IsNullOrEmpty(newPlate) && newPlate != currentPlate && !UtilityHelper.IsPlateAlreadyUsed(newPlate))
-                                //    {
-                                //        Console.WriteLine($"[LicensePlateChanger]: Applying new license plate format {newPlate} to vehicle {val.DisplayName}.");
-
-                                //        val.Mods.LicensePlate = newPlate;
-
-                                //        Globals.vehicleLicensePlates[vehicleID] = newPlate;
-                                //    }
-                                //}
-                                //else
-                                //{
-                                //    // Skip processing this vehicle if it's excluded
-                                //    continue;
-                                //}
-                            }
-                        }
-                    }
-                }
-            }
         }
 
         private void OnAborted(object sender, EventArgs e)
---
+++ Threads/VehicleManager.cs
@@ -0,0 +1,21 @@
+﻿using GTA;
+using System;
+
+namespace LicensePlateChanger.Threads
+{
+    [ScriptAttributes(NoDefaultInstance = true)]
+    public class VehicleManager : Script
+    {
+        public VehicleManager()
+        {
+            Tick += OnTick;
+
+            Interval = 1000;
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            Console.WriteLine("Tick 2");
+        }
+    }
+}

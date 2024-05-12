fd383e676eaa814cefce6cd4bfb52ab7f6c28beb
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-11 21:28:29 -0700

    New Updater for Tick System

---
+++ Utils/Updater.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LicensePlateChanger.Utils
+{
+    public abstract class Updater
+    {
+        #region Constructors
+        protected Updater(int startTick = 0)
+        {
+            Tick = startTick;
+            StartTick = startTick;
+        }
+        #endregion
+
+        #region Public Methods
+        public void OnTick()
+        {
+            OnUpdate(Tick);
+
+            if (ActionQueue.ContainsKey(Tick))
+                ActionQueue[Tick].DynamicInvoke();
+
+            if (Tick == 0 || Tick == StartTick)
+                OnFirstUpdate();
+
+            if (IsActive)
+            {
+                InactiveTicks = 0;
+
+                OnActiveUpdate(ActiveTicks, Tick);
+
+                if (ActiveTicks == 0)
+                    OnFirstActiveUpdate(Tick);
+
+                ActiveTicks++;
+            }
+
+            if (!IsActive)
+            {
+                ActiveTicks = 0;
+
+                OnInactiveUpdate(InactiveTicks, Tick);
+
+                if (InactiveTicks == 0)
+                    OnFirstInactiveUpdate(Tick);
+
+                InactiveTicks++;
+            }
+
+            Tick++;
+        }
+        #endregion
+
+        #region Abstract Methods
+        /// <summary>
+        /// Fired every time a tick has been made.
+        /// </summary>
+        /// <param name="tick">The total amount of ticks.</param>
+        protected abstract void OnUpdate(int tick);
+
+        /// <summary>
+        /// Fired everytime a tick has been made while active.
+        /// </summary>
+        /// <param name="activeTick">The current tick on this activity state.</param>
+        /// <param name="tick">The total amount of ticks.</param>
+        protected abstract void OnActiveUpdate(int activeTick, int tick);
+        #endregion
+
+        #region Virtual Methods
+        /// <summary>
+        /// Fired on the absolute first update.
+        /// </summary>
+        protected virtual void OnFirstUpdate()
+        { }
+
+        /// <summary>
+        /// Fired everytime a tick has been made while inactive.
+        /// </summary>
+        /// <param name="inactiveTick">The current tick on this activity state.</param>
+        /// <param name="tick">The total amount of ticks.</param>
+        protected virtual void OnInactiveUpdate(int inactiveTick, int tick)
+        { }
+
+        /// <summary>
+        /// Fired the first time an active update has been made.
+        /// </summary>
+        /// <param name="tick">The total amount of ticks.</param>
+        protected virtual void OnFirstActiveUpdate(int tick)
+        { }
+
+        /// <summary>
+        /// Fired the first time an inactive update has been made.
+        /// </summary>
+        /// <param name="tick">The total amount of ticks.</param>
+        protected virtual void OnFirstInactiveUpdate(int tick)
+        { }
+        #endregion
+
+        #region Properties
+        public Dictionary<int, Action> ActionQueue { get; set; } = new Dictionary<int, Action>();
+
+        /// <summary>
+        /// Returns the tick of which the ticking started.
+        /// This isn't always 0, as the StartTick can be set.
+        /// </summary>
+        public int StartTick { get; }
+
+        /// <summary>
+        /// Returns how many times this Updating instance has ticked.
+        /// </summary>
+        public int Tick { get; private set; }
+
+        /// <summary>
+        /// If active, returns how many times it has been ticked during its session.
+        /// </summary>
+        public int ActiveTicks { get; private set; }
+
+        /// <summary>
+        /// If inactive, returns how many times it has been ticked during its session.
+        /// </summary>
+        public int InactiveTicks { get; private set; }
+
+        /// <summary>
+        /// Returns how many seconds this updater has been alive.
+        /// </summary>
+        public int SecondsAlive => Tick / 1000;
+
+        /// <summary>
+        /// Returns whether this Updating instance is active.
+        /// </summary>
+        public bool IsActive { get; protected set; } = true;
+        #endregion
+    }
+}

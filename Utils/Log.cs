176e3f7a66949404750f73d0facb9f2a306a8704
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-11 19:54:25 -0700

    New Logging System

---
+++ Utils/Log.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+
+namespace LicensePlateChanger.Utils
+{
+    internal enum LogLevel
+    {
+        DEBUG, // Only prints if DEBUG flag is enabled
+        INFO, // Prints to log file only
+        DEVMODE, // Prints to console aswell
+        ERROR, // Prints to console aswell
+        FATAL // Prints to console aswell
+    }
+
+    internal static class Log
+    {
+        private const string Path = "Scripts\\LicensePlateChanger.log";
+
+        static Log()
+        {
+            var message = $"LPC - License Plate Changer v{Assembly.GetExecutingAssembly().GetName().Version}";
+            message += Environment.NewLine;
+            message += "-----------------------------------------------------------";
+            message += Environment.NewLine;
+
+            var writer = new StreamWriter(Path, false);
+            writer.WriteLine(message);
+            writer.Close();
+        }
+
+        public static void Write(string text, LogLevel logLevel = LogLevel.INFO)
+        {
+#if !DEBUG
+             if (logLevel == LogLevel.DEBUG) return;
+#endif
+            if (logLevel > LogLevel.INFO) Console.WriteLine($"[{logLevel}] {text}");
+
+            var writer = new StreamWriter(Path, true);
+            writer.WriteLine($"[{DateTime.Now.ToString(CultureInfo.InvariantCulture)}] [{logLevel}] {text}");
+            writer.Close();
+        }
+    }
+
+    internal static class LogExtensions
+    {
+        public static void ToLog(this string message, LogLevel logLevel = LogLevel.INFO) =>
+            Log.Write(message, logLevel);
+    }
+}

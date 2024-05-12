5be8d0f5e2b6974933f3ce187227b99669550647
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-11 19:40:12 -0700

    New Globals file

---
+++ Globals.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LicensePlateChanger
+{
+    public static class Globals
+    {
+        public static Dictionary<int, string> vehicleLicensePlates = new Dictionary<int, string>();
+    }
+}

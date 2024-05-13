79c967edae4c6897c00ef8c2ceaa0c2bbff80aaa
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-13 00:36:31 -0700

    remove datamember names

---
+++ Models/VehicleData.cs
@@ -0,0 +1,12 @@
+﻿namespace LicensePlateChanger.Models
+{
+    public class VehicleClass
+    {
+        public bool isEnabled { get; set; }
+    }
+
+    public class VehicleData
+    {
+        public VehicleClasses VehicleClass { get; set; }
+    }
+}

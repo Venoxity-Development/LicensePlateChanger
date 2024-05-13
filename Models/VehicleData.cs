103230722e5de1389de8dbe77b21d9cbb505643e
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-13 01:48:08 -0700

    Model Renaming stuff

--- Models/VehicleData.cs
+++ Models/VehicleData.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace LicensePlateChanger.Models
 {
-    public class vehicleClass
+    public class VehicleClassOptions
     {
         public bool isEnabled { get; set; }
     }
 
-    public class vehicleData
+    public class VehicleData
     {
-        public Dictionary<string, vehicleClass> vehicleClass { get; set; }
+        [DataMember(Name = "vehicleClass")]
+        public Dictionary<string, VehicleClassOptions> VehicleClassOptions { get; set; }
     }
 }

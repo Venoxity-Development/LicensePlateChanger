962d78e93f05b1001d94344d1db42b44e8e3557f
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-12 23:04:55 -0700

    New Vehicle Classes Model

---
+++ Models/VehicleClasses.cs
@@ -0,0 +1,14 @@
+﻿namespace LicensePlateChanger.Models
+{
+    public class VehicleClasses
+    {
+        public VehicleClass Cars { get; set; }
+        public VehicleClass Motorcycles { get; set; }
+        public VehicleClass Industrial { get; set; }
+        public VehicleClass Utility { get; set; }
+        public VehicleClass Service { get; set; }
+        public VehicleClass Emergency { get; set; }
+        public VehicleClass Military { get; set; }
+        public VehicleClass Commercial { get; set; }
+    }
+}

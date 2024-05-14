031969a2805ebf753fac7d32a26e4a72cb653bdc
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-13 22:55:34 -0700

    Add method doesnt do anything yet but prepared extension

--- Extensions/VehicleExtensions.cs
+++ Extensions/VehicleExtensions.cs
@@ -1,6 +1,15 @@
-﻿namespace LicensePlateChanger.Extensions
+﻿using GTA;
+using GTA.Native;
+
+namespace LicensePlateChanger.Extensions
 {
     internal static class VehicleExtensions
     {
+        public static bool IsVehicleExcluded(Vehicle vehicle)
+        {
+            // Coming Soon
+
+            return false;
+        }
     }
 }

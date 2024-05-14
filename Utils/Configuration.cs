abd20ced79dc055a52a845382d9f781c25ec6b8c
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-13 22:13:37 -0700

    Fixed validation system and setup new enums

--- Utils/Configuration.cs
+++ Utils/Configuration.cs
@@ -7,7 +7,7 @@ namespace LicensePlateChanger.Utils
     public class Configuration
     {
         #region Properties
-        public static vehicleData ConfigurationData { get; private set; }
+        public static VehicleData ConfigurationData { get; private set; }
         public static Dictionary<string, VehicleClass> VehicleClassMapping { get; private set; }
         public static VehicleClassMappingValidationState ValidationState { get; private set; }
         #endregion
@@ -39,54 +39,42 @@ namespace LicensePlateChanger.Utils
 
         private static VehicleClassMappingValidationState ValidateVehicleClassMapping()
         {
-            ValidationState = VehicleClassMappingValidationState.Success;
+            "Validating vehicle class mapping...".ToLog();
+
+            if (ConfigurationData.VehicleClassOptions == null)
+            {
+                "No vehicle class mapping found.".ToLog(LogLevel.ERROR);
+                ValidationState = VehicleClassMappingValidationState.FailureNoMapping;
+                return ValidationState;
+            }
+
+            var vehicleClassMapping = new Dictionary<string, VehicleClass>();
+
+            foreach (var classEntry in ConfigurationData.VehicleClassOptions)
+            {
+                var className = classEntry.Key.ToString();
+                if (Enum.TryParse(className, true, out VehicleClass vehicleClassEnum))
+                {
+                    // VehicleClass.Cars
+                    for (int i = 0; i < VehicleClassMappings.classNames.Length; i++)
+                    {
+                        vehicleClassMapping[VehicleClassMappings.classNames[i]] = VehicleClassMappings.classValues[i];
+                    }
+
+                    // Regular Classes
+                    vehicleClassMapping[className] = vehicleClassEnum;
+                }
+                else
+                {
+                    $"Invalid vehicle class name: {className}".ToLog(LogLevel.ERROR);
+                    ValidationState = VehicleClassMappingValidationState.FailureInvalidClassName; 
+                    return ValidationState;
+                }
+            }
+
+            "Vehicle class mapping validation completed successfully. All vehicle classes have been validated and mapped.".ToLog();
+            ValidationState = VehicleClassMappingValidationState.Success; 
             return ValidationState;
-
-            //"Validating vehicle class mapping...".ToLog();
-
-            //VehicleClassMapping = new Dictionary<string, VehicleClass>();
-
-            //string[] classNames = { "Compacts", "Sedans", "SUVs", "Coupes", "Muscle", "SportsClassics", "Sports", "Super", "OffRoad", "Vans" };
-            //VehicleClass[] classValues = { VehicleClass.Compacts, VehicleClass.Sedans, 
-            //    VehicleClass.SUVs, VehicleClass.Coupes, VehicleClass.Muscle, VehicleClass.SportsClassics, 
-            //    VehicleClass.Sports, VehicleClass.Super, VehicleClass.OffRoad, VehicleClass.Vans };
-
-            //if (ConfigurationData.TryGetValue("vehicleClass", out var vehicleClassTable) && vehicleClassTable is TomlTable)
-            //{
-            //    var vehicleClass = (TomlTable)vehicleClassTable;
-
-            //    foreach (var classEntry in vehicleClass)
-            //    {
-            //        var className = classEntry.Key.ToString();
-            //        if (Enum.TryParse(className, true, out VehicleClass vehicleClassEnum))
-            //        {
-            //            // VehicleClass.Cars
-            //            for (int i = 0; i < classNames.Length; i++)
-            //            {
-            //                VehicleClassMapping[classNames[i]] = classValues[i];
-            //            }
-
-            //            // Regular Classes
-            //            VehicleClassMapping[className] = vehicleClassEnum;
-            //        }
-            //        else
-            //        {
-            //            $"Invalid vehicle class name: {className}".ToLog(LogLevel.ERROR);
-            //            ValidationState = VehicleClassMappingValidationState.FailureInvalidClassName; 
-            //            return ValidationState;
-            //        }
-            //    }
-
-            //    "Vehicle class mapping validation completed successfully. All vehicle classes have been validated and mapped.".ToLog();
-            //    ValidationState = VehicleClassMappingValidationState.Success; 
-            //    return ValidationState;
-            //}
-            //else
-            //{
-            //    "No vehicle class mapping found.".ToLog(LogLevel.ERROR);
-            //    ValidationState = VehicleClassMappingValidationState.FailureNoMapping; 
-            //    return ValidationState;
-            //}
         }
         #endregion
     }

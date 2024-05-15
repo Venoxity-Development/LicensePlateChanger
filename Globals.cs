a380454f90a6807e53a0cba8d454f3dd860a2b74
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-15 02:08:24 -0700

    added global for plate types

--- Globals.cs
+++ Globals.cs
@@ -5,5 +5,6 @@ namespace LicensePlateChanger
     public static class Globals
     {
         public static Dictionary<int, string> vehicleLicensePlates = new Dictionary<int, string>();
+        public static Dictionary<int, int> vehicleLicenseClassName = new Dictionary<int, int>();
     }
 }

05189e33090b190b290781b33a237442e5562f8f
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-05-14 23:53:16 -0700

    Added Check configuration on vehicle helper

--- Utils/ConfigurationHelper.cs
+++ Utils/ConfigurationHelper.cs
@@ -1,4 +1,6 @@
-﻿using LicensePlateChanger.Models;
+﻿using GTA;
+using LicensePlateChanger.Extensions;
+using LicensePlateChanger.Models;
 using System;
 using System.IO;
 using Tomlyn;
@@ -25,6 +27,30 @@ namespace LicensePlateChanger.Utils
                 $"Error loading TOML file: {ex.Message}".ToLog(LogLevel.ERROR);
                 return null;
             }
+        }
+
+        public static VehicleClassOptions CheckVehicleConfiguration(Vehicle vehicle)
+        {
+            if (Enum.TryParse(VehicleExtensions.GetClassNameForVehicle(vehicle), out VehicleClass targetClass))
+            {
+                if (Configuration.ConfigurationData.VehicleClassOptions.TryGetValue(targetClass, out var classOptions))
+                {
+                    if (classOptions.isEnabled)
+                    {
+                        int vehicleID = vehicle.Handle;
+                        if (!Globals.vehicleLicensePlates.ContainsKey(vehicleID))
+                        {
+                            return classOptions;
+                        }
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }

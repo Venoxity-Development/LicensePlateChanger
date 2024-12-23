65676f1f841b0108acda82fbb03a5452acae4680
GravityScriptsV2 <95504918+GravityScriptsV2@users.noreply.github.com>
2024-12-23 02:42:14 -0800

    Added VehicleTypeOptions to VehicleData

--- Engine/Data/VehicleData.cs
+++ Engine/Data/VehicleData.cs
@@ -9,9 +9,19 @@ namespace LicensePlateChanger.Engine.Data
         public List<PlateSet> plateSets { get; set; }
     }
 
+    public class VehicleTypeOptions
+    {
+        public bool isTypeEnabled { get; set; }
+        public List<PlateSet> plateSets { get; set; }
+        public List<string> allowedVehicles { get; set; }
+    }
+
     public class VehicleData
     {
         [DataMember(Name = "vehicleClass")]
         public Dictionary<VehicleClass, VehicleClassOptions> VehicleClassOptions { get; set; }
+
+        [DataMember(Name = "vehicleType")]
+        public Dictionary<string, VehicleTypeOptions> VehicleTypeOptions { get; set; }
     }
 }
